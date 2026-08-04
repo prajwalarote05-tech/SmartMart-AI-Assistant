67a01849c4713557ac77eff3bd94bbd255096894
prajwalarote05-tech <prajwalarote05@gmail.com>
2026-08-04 17:32:08 +0530

    Added SmartMart AI Assistant files

---
+++ app.py
@@ -0,0 +1,93 @@
+import os
+import gradio as gr
+
+from langchain_groq import ChatGroq
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_chroma import Chroma
+
+# -----------------------------
+# API Key
+# -----------------------------
+GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+
+llm = ChatGroq(
+    groq_api_key=GROQ_API_KEY,
+    model_name="llama-3.3-70b-versatile"
+)
+
+# -----------------------------
+# Load PDF
+# -----------------------------
+loader = PyPDFLoader("SmartMart Policy.pdf")
+documents = loader.load()
+
+# -----------------------------
+# Split PDF
+# -----------------------------
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=200
+)
+
+docs = text_splitter.split_documents(documents)
+
+# -----------------------------
+# Embeddings
+# -----------------------------
+embeddings = HuggingFaceEmbeddings(
+    model_name="sentence-transformers/all-MiniLM-L6-v2"
+)
+
+vector_db = Chroma.from_documents(
+    documents=docs,
+    embedding=embeddings
+)
+
+retriever = vector_db.as_retriever(search_kwargs={"k":3})
+
+# -----------------------------
+# Chat Function
+# -----------------------------
+def smartmart_chat(question):
+
+    docs = retriever.invoke(question)
+
+    context = "\n\n".join([doc.page_content for doc in docs])
+
+    prompt = f"""
+You are SmartMart AI Assistant.
+
+Context:
+{context}
+
+Question:
+{question}
+
+Answer ONLY using the context.
+If the answer is not available in the PDF, reply:
+Sorry, I couldn't find that information in the SmartMart knowledge base.
+"""
+
+    response = llm.invoke(prompt)
+
+    return response.content
+
+# -----------------------------
+# Gradio UI
+# -----------------------------
+demo = gr.Interface(
+    fn=smartmart_chat,
+    inputs=gr.Textbox(
+        lines=2,
+        placeholder="Ask anything about SmartMart..."
+    ),
+    outputs="text",
+    title="🛒 SmartMart AI Assistant",
+    description="Ask questions about SmartMart Policy, Orders, Refunds, Shipping and Products."
+)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 7860))
+    demo.launch(server_name="0.0.0.0", server_port=port)
